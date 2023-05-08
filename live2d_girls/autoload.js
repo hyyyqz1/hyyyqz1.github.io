152139d8e0678b23e9c3a4eafcde2bcf07745aae
HYYYQZ <132748986+HYYYQZ@users.noreply.github.com>
2023-05-08 17:44:31 +0800

    Update autoload.js

--- live2d_girls/autoload.js
+++ live2d_girls/autoload.js
@@ -34,7 +34,7 @@ function loadExternalResource(url, type) {
 	]).then(() => {
 		initWidget({
 			waifuPath: tips_path + "waifu-tips.json",
-			apiPath: "/live2d/",
+			apiPath: "https://home.hyyyqz.top/live2d/",
 			//cdnPath: "https://你的对象存储域名/live2d/"
 		});
 	});
